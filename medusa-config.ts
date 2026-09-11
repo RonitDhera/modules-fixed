acc918465174c7c7142aabd3949e8c8d8d15e8d8
Ronit Dhera <ronitdheraworks@gmail.com>
2026-09-11 14:04:52 -0700

    deployment

--- medusa-config.ts
+++ medusa-config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
-  admin: {
-    disable: true, 
-  },
+  // admin: {
+  //   disable: true, 
+  // },
 })
