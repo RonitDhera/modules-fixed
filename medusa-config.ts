e8b62e7c740f162fd434b33324507da480f5729f
Ronit Dhera <ronitdheraworks@gmail.com>
2026-09-15 17:05:03 -0700

    ..

--- medusa-config.ts
+++ medusa-config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       resolve: "./src/modules/inventory-transfer",
     },
     {
-      resolve: "./src/modules/google-integration", // stop
+      resolve: "./src/modules/google-integration", // 
     },
     {
   resolve: "./src/modules/analytics",
@@ -49,16 +49,16 @@ export default defineConfig({
       },
     },
   ],
-  projectConfig: {
-    databaseUrl: process.env.DATABASE_URL || required("DATABASE_URL"),
-    http: {
-      storeCors: process.env.STORE_CORS || "http://localhost:8000",
-      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
-      authCors: process.env.AUTH_CORS || "http://localhost:9000",
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    },
+projectConfig: {
+  databaseUrl: process.env.DATABASE_URL || required("DATABASE_URL"),
+  http: {
+    storeCors: process.env.STORE_CORS || "http://localhost:8000",
+    adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
+    authCors: process.env.AUTH_CORS || "http://localhost:9000",
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
   },
+},
   admin: {
     disable: process.env.DISABLE_ADMIN === "true",
     path: "/",
